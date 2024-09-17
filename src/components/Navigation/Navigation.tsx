e24ca3a5e477901b0d67d38f91427ddfa1ce5bfd
spillingweb <khspilling@gmail.com>
2024-09-17 09:50:44 +0200

    made js for navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,30 +1,60 @@
+import { useState } from "react";
 import styles from "./Navigation.module.scss";
 
+type NavLink = {
+  href: string;
+  text: string;
+};
+
+const NAVLINKS: NavLink[] = [
+  {
+    href: "#about",
+    text: "About Natours",
+  },
+  {
+    href: "#features",
+    text: "Your benefits",
+  },
+  {
+    href: "#tours",
+    text: "Popular Tours",
+  },
+  {
+    href: "#stories",
+    text: "Stories",
+  },
+  {
+    href: "#booking",
+    text: "Booking",
+  },
+];
+
 const Navigation: React.FC = () => {
+  const [navOpen, setNavOpen] = useState<boolean>(false);
+
+  function toggleHamburgerMenu() {
+    setNavOpen((prev) => !prev);
+  }
+
+  const navClass = navOpen ? styles.navOpen : "";
+
   return (
-    <div id="navigation">
-      <input type="checkbox" id="navCheck" className={styles.navCheckbox} />
-      <label htmlFor="navCheck" className={styles.hamburger}>
-          <span className={styles.line}>&nbsp;</span>
-      </label>
+    <div id="navigation" className={navClass}>
+      <div
+        tabIndex={0}
+        className={styles.hamburger}
+        onClick={toggleHamburgerMenu}
+      >
+        <span className={styles.line}>&nbsp;</span>
+      </div>
       <div className={styles.navBackground}>&nbsp;</div>
       <nav className={styles.navigation}>
         <ul className={styles.navList}>
-          <li>
-            <a href="#about">About Natours</a>
-          </li>
-          <li>
-            <a href="#features">Your Benefits</a>
-          </li>
-          <li>
-            <a href="#tours">Popular Tours</a>
-          </li>
-          <li>
-            <a href="#stories">Stories</a>
-          </li>
-          <li>
-            <a href="#booking">Book Now</a>
-          </li>
+          {NAVLINKS.map((navLink) => (
+            <li key={navLink.href} onClick={toggleHamburgerMenu}>
+              <a href={navLink.href}>{navLink.text}</a>
+            </li>
+          ))}
         </ul>
       </nav>
     </div>
